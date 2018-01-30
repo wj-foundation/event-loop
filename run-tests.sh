763938380e0661c9c514c88c82d8b8d51565a3b4
wangjian <1636801376@qq.com>
2018-01-30 18:20:48 +0800

    fix

--- run-tests.sh
+++ run-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev
 
-if [ "${LIBEVENT_DISABLED}" = "true" ]; then
-    phpenv config-rm libevent.ini
+if [ "{$LIBEVENT_ENABLED}" = "false" ]; then
     phpunit tests/CreateStreamSelectLoopTest.php
 else
+    phpenv config-add libevent.ini
     phpunit tests/CreateLibEventLoopTest.php
 fi
